c523585fca0f6c2d316906cf1a84f7192dc065ac
ganeshmunishetty <ganesh.munishetty@gmail.com>
2023-09-22 19:32:53 +0530

    Order A Product

---
+++ Model/OrderedProduct.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Order.Model
+{
+    public class OrderedProduct
+    {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+      
+    }
+}
